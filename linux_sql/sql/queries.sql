abd5bbaa9afe2ce7598b0571ce5e8516941a0c36
Homa Almasieh <halmasieh@yahoo.co.uk>
2021-09-17 04:59:47 +0000

    Updating queries

--- linux_sql/sql/queries.sql
+++ linux_sql/sql/queries.sql
@@ -12,52 +12,22 @@ ORDER BY
    cpu_number ASC,
    total_mem  DESC;
 
---Create  a function that returns the timestamp rounded to the nearest 5 minute interval.
-CREATE FUNCTION round5(ts timestamp) RETURNS timestamp AS
-$$
-BEGIN
-    RETURN date_trunc('hour', ts) + date_part('minute', ts):: int / 5 * interval '5 min';
-END;
-$$
-    LANGUAGE PLPGSQL;
-
---Verify round5 function
-SELECT
-host_id, timestamp, round5(timestamp)
-FROM host_usage;
-
---Create a function that gets free_memory and host_id and returns used memory percentage.
-CREATE FUNCTION used_memory_percentage(free_memory INTEGER, host_id INTEGER) RETURNS NUMERIC AS
-$$
-DECLARE
-    total_memory INTEGER;
-BEGIN
-    SELECT (total_mem / 1000)
-    INTO total_memory
-    FROM host_info
-    WHERE host_info.id = host_usage.host_id;
-    RETURN (total_memory - memory_free) / total_memory * 100 ;
-END;
-$$
-    LANGUAGE PLPGSQL;
-
 --Average used memory in percentage over 5 minutes interval for each host.
-select
-	host_usage.host_id,
-	host_info.hostname,
-	round5(
-      CAST(host_usage.timestamp AS timestamp
-   ) As timestamp,
-   AVG(used_memory_percentage(memory_free, host_id)
-   ) AS avg_used_mem_percentage
+SELECT
+  id,
+  hostname,
+  date_trunc('hour', hu.timestamp) + date_part('minute', hu.timestamp)::int / 5 * interval '5 min' AS interval,
+  AVG((total_mem - memory_free) * 100 / total_mem)::int AS percentage_used
 FROM
-   host_usage
+  host_info hi JOIN
+    host_usage hu ON
+    hi.id = hu.host_id
 GROUP BY
-   host_usage.host_id,
-   round5(
-      CAST(timestamp As timestamp)
-   )
-   LIMIT 5;
+  id,
+  interval
+ORDER BY
+  id,
+  interval;
 
    --Detect host failure for less than three data points within 5-min interval.
 SELECT
