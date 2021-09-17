be7989909edf59acb86b10b886bc87e5d08eaf26
Homa Almasieh <halmasieh@yahoo.co.uk>
2021-09-17 15:17:59 +0000

    Updating the script

--- linux_sql/script/host_info.sh
+++ linux_sql/script/host_info.sh
@@ -9,8 +9,7 @@ psql_password=$5
 
 #check the correct number of arguments
 if [ "$#" -ne 5 ]; then
-   echo "host_info: invalid arguments: require 5 arguments" >&2
-   echo "usage: ./host_info.sh psql_host psql_port db_name psql_user psql_password" >&2
+   echo "Error: Invalid number of arguments."
    exit 1
  fi
 
