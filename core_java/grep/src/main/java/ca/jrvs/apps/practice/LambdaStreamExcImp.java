1f4472c0cdee2e6b2ed972ed59a60c108da2c52d
Homa Almasieh <halmasieh@yahoo.co.uk>
2021-10-13 14:09:27 +0000

    Adding practice

---
+++ core_java/grep/src/main/java/ca/jrvs/apps/practice/LambdaStreamExcImp.java
@@ -0,0 +1,11 @@
+package ca.jrvs.apps.practice;
+
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class LambdaStreamExcImp {
+
+}
