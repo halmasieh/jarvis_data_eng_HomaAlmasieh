dfec4372779c660aeea14ccc89796df396ab7b1f
Homa Almasieh <halmasieh@yahoo.co.uk>
2021-11-13 04:43:30 +0000

    Adding StackUsingQueue

---
+++ practice/src/main/java/ca/jrvs/practice/codingChallenge/StackUsingQueue.java
@@ -0,0 +1,65 @@
+package ca.jrvs.practice.codingChallenge;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Implement Stack Using Queue
+ * https://www.notion.so/jarvisdev/Implement-Stack-using-Queue-a5e8c3e5edc244e885f2f4735e82fc0f
+ *
+ * Push :
+ * If queue1 is empty, add elements to queue1, Time Complexity: O(1)
+ * If queue1 is not empty, add all elements of queue1 to queue2 , add current element to queue1 and copy all elements of queue2 to queue1. Time Complexity: O(n)
+ * Pop : Simply remove element from queue1.
+ *
+ */
+public class StackUsingQueue {
+
+  Queue<Integer> queue1;
+  Queue<Integer> queue2;
+
+  public StackUsingQueue() {
+    queue1 = new LinkedList<Integer>();
+    queue2 = new LinkedList<Integer>();
+  }
+
+  // Remove value from the beginning of the list for demonstrating behaviour of stack
+  public void push(int i) {
+    // If queue1 is empty, add elements to queue1
+    if (queue1.size() == 0)
+      queue1.add(i);
+    else {
+      int sizeOfQueue1 = queue1.size();
+      // Copy elements of Queue1 to Queue2
+      for (int j = 0; j < sizeOfQueue1; j++)
+        queue2.add(queue1.remove());
+      queue1.add(i);
+      // Copy elements for Queue2 to Queue1
+      for (int k = 0; k < sizeOfQueue1; k++)
+        queue1.add(queue2.remove());
+    }
+  }
+  public int pop(){
+    if(queue1.size()==0) {
+      throw new QueueEmptyException("Queue is empty!");
+    }
+    return queue1.remove();
+  }
+  /**
+   * Exception to indicate that Queue is empty.
+   */
+
+  class QueueEmptyException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public QueueEmptyException() {
+      super();
+    }
+
+    public QueueEmptyException(String message) {
+      super(message);
+    }
+  }
+
+}
+
---
+++ practice/src/test/java/ca/jrvs/practice/codingChallenge/StackUsingQueueTest.java
@@ -0,0 +1,26 @@
+package ca.jrvs.practice.codingChallenge;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class StackUsingQueueTest {
+
+  @Test
+  public void StackUsingQueue() {
+    StackUsingQueue stack = new StackUsingQueue();
+    stack.push(20);
+    stack.push(40);
+    stack.push(70);
+    stack.push(50);
+    stack.push(90);
+    stack.push(110);
+    stack.push(30);
+    int expected1 = 30;
+    Assert.assertEquals(expected1, stack.pop());
+    stack.push(170);
+    int expected2 = 170;
+    Assert.assertEquals(expected2, stack.pop());
+  }
+}
